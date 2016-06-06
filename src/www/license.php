4808bfce3b8c02eb3db02c1a6705924e45796b81
Paulius Dambravičius <dambravicius@gmail.com>
2016-06-06 20:15:30 +0300

    Update license

--- src/www/license.php
+++ src/www/license.php
@@ -38,7 +38,7 @@ include("head.inc");
   <div class="container-fluid ">
     <div class="row">
       <section class="col-xs-11">
-        <p><a href="https://opnsense.org/" target="_blank">OPNsense&reg;</a></span> is Copyright &copy; <?=$g['product_copyright_years'];?> <?= $g['product_copyright_owner'] ?><br>All rights reserved.</p>
+        <p><a href="http://www.smart-soft.ru/" target="_blank">TING&reg;</a></span> is Copyright &copy; <?=$g['product_copyright_years'];?> by Smart-Soft Ltd. All rights reserved.</p>
         <p>Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:</p>
         <p><ol><li>Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.</li>
         <li>Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.</li></ol></p>
@@ -52,11 +52,10 @@ include("head.inc");
         WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
         OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
         ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.</p>
-        <p>OPNsense is based on <a href="http://www.freebsd.org" target="_blank">FreeBSD</a> Copyright &copy; The FreeBSD Project. All rights reserved.</p>
-        <p>OPNsense is a fork of <a href="http://www.pfsense.org" target="_blank">pfSense&reg;</a> <small>(Copyright &copy; 2004-2014 Electric Sheep Fencing, LLC. All rights reserved.)</small> a fork from <a href="http://m0n0.ch/wall" target="_blank">m0n0wall&reg;</a> <small>(Copyright &copy; 2002-2013 Manuel Kasper)</small>.</p>
-        <p>OPNsense includes various freely available software packages and ports.
+        <p>TING is based on <a href="http://www.freebsd.org" target="_blank">FreeBSD</a> Copyright &copy; The FreeBSD Project. All rights reserved.</p>
+        <p>TING is a fork of <a href="http://www.opnsense.org" target="_blank">OPNsense</a> Copyright &copy; Deciso B.V. All rights reserved.</p>
+        <p>TING includes various freely available software packages and ports.
         The incorporated third party tools are listed <a href="/ui/core/firmware/">here</a>.</p>
-        <p>The authors of OPNsense would like to thank all contributors for their efforts.</p>
       </section>
     </div>
   </div>
