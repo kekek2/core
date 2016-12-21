47935b27c1ba68446d8ba0e99cfccc193c0373a2
Alexander Shursha <kekek2@ya.ru>
2016-12-21 10:47:52 +0300

    Progress checking control sums

--- src/etc/checksum.php
+++ src/etc/checksum.php
@@ -3,30 +3,36 @@
 require_once("util.inc");
 require_once("config.inc");
 
+$descriptorspec = array(
+   0 => array("pipe", "r"),
+   1 => array("file", "php://stdout", "w"),
+   2 => array("pipe", "w")
+);
+
 $commands = [
     "kernel" => "/usr/local/bin/cfv -VV -f boot/kernel.sum",
     "base" => "/usr/local/bin/cfv -VV -f boot/base.sum",
-    "packages" => "/usr/sbin/pkg check -qs"
+    "packages" => "/usr/sbin/pkg check -s"
     ];
 
 chdir("/");
 
 foreach ($commands as $what => $command)
 {
-    $fp = popen($command . " 2>&1", 'r');
+    $process = proc_open($command, $descriptorspec, $pipes);
     if ($what != "packages")
-        fread($fp, 1024);
+        fread($pipes[2], 1024);
     $err = false;
-    while (!feof($fp))
+    while (!feof($pipes[2]))
     {
-        $str = substr(fread($fp, 1024), 0, -1);
+        $str = substr(fread($pipes[2], 1024), 0, -1);
         if (strlen($str) > 0)
         {
             $err = true;
             syslog(LOG_ERR, $str);
         }
     }
-    pclose($fp);
+    proc_close($process);
     if ($err)
         mark_subsystem_dirty($what);
 }
