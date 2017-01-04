7d3b3e4212397387aba49e510f329257aaf8ee54
Alexander Shursha <kekek2@ya.ru>
2017-01-04 10:10:29 +0300

    Fix checksum visualization.

--- src/etc/checksum.php
+++ src/etc/checksum.php
@@ -4,31 +4,52 @@ require_once("config.inc");
 require_once("notices.inc");
 
 $descriptorspec = array(
-   0 => array("pipe", "r"),
-   1 => array("file", "php://stdout", "w"),
-   2 => array("pipe", "w")
+    0 => array("pipe", "r"),
+    1 => array("pipe", "w"),
+    2 => array("pipe", "w")
 );
 
 $commands = [
-    "kernel" => "/usr/local/bin/cfv -VV -f boot/kernel.sum",
-    "base" => "/usr/local/bin/cfv -VV -f boot/base.sum",
+    "kernel" => "/usr/local/bin/cfv -VV --progress yes -f boot/kernel.sum",
+    "base" => "/usr/local/bin/cfv -VV --progress yes -f boot/base.sum",
     "packages" => "/usr/sbin/pkg check -s"
-    ];
+];
 
 chdir("/");
 
 foreach ($commands as $what => $command)
 {
+    echo "Checking " . $what;
     $process = proc_open($command, $descriptorspec, $pipes);
+    $read = array($pipes[1], $pipes[2]);
+    $write = NULL;
+    $except = NULL;
+
     if ($what != "packages")
         fgets($pipes[2], 1024);
-    while (!feof($pipes[2]))
+    while (true)
     {
-        $str = substr(fgets($pipes[2], 1024), 0, -1);
-        if ($str !== false)
-            file_notice($what, $str, $priority = 2);
+        $read = array($pipes[1], $pipes[2]);
+        if (stream_select($read, $write, $except, null) === false)
+            break;
+        if (feof($pipes[1]) && feof($pipes[2]))
+            break;
+        foreach ($read as $key => $pipe)
+        {
+            if ($key == 0)
+            {
+                $str = fread($pipe, 1024);
+                echo ".";
+            }
+            else
+            {
+                $str = fgets($pipe, 1024);
+                if ($str !== false)
+                    file_notice($what, $str, $priority = 1);
+            }
+        }
     }
-    fclose($pipes[2]);
     proc_close($process);
+    echo "done\n";
 }
 
