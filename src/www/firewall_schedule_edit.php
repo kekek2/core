faca10b7ca8e90f9f5ac86bf14a2d5e685e32867
Paulius Dambravičius <dambravicius@gmail.com>
2017-01-22 20:09:23 +0200

    firewall_scrub

--- src/www/firewall_schedule_edit.php
+++ src/www/firewall_schedule_edit.php
@@ -788,7 +788,7 @@ function removeRow(el) {
             <div class="content-box tab-content">
               <form method="post" name="iform" id="iform">
                 <input type="hidden" name="schedlabel" value="<?=$pconfig['schedlabel'];?>"/>
-                  <table class="table table-striped opnsense_standard_table_form">
+                  <table class="table table-clean-form opnsense_standard_table_form">
                     <tbody>
                       <tr>
                         <td width="15%"><strong><?=gettext("Schedule information");?></strong></td>
--- src/www/firewall_scrub_edit.php
+++ src/www/firewall_scrub_edit.php
@@ -266,7 +266,7 @@ include("head.inc");
             <section class="col-xs-12">
               <div class="content-box">
                 <div class="table-responsive">
-                  <table class="table table-striped opnsense_standard_table_form">
+                  <table class="table table-clean-form opnsense_standard_table_form">
                   <tr>
                     <td valign="top"><strong><?=gettext("Edit Firewall scrub rule");?></strong></td>
                     <td align="right">
@@ -549,7 +549,7 @@ include("head.inc");
           <section class="col-xs-12">
             <div class="content-box">
               <div class="table-responsive">
-                <table class="table table-striped opnsense_standard_table_form">
+                <table class="table table-clean-form opnsense_standard_table_form">
                   <tr>
                     <td colspan="2"><strong><?=gettext("Normalizations");?></strong></td>
                   </tr>
@@ -626,7 +626,7 @@ include("head.inc");
           <section class="col-xs-12">
             <div class="content-box">
               <div class="table-responsive">
-                <table class="table table-striped opnsense_standard_table_form">
+                <table class="table table-clean-form opnsense_standard_table_form">
 <?php
                     $has_created_time = (isset($a_scrub[$id]['created']) && is_array($a_scrub[$id]['created']));
                     $has_updated_time = (isset($a_scrub[$id]['updated']) && is_array($a_scrub[$id]['updated']));
