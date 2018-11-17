c65ea816b7cbf28a1eecb342a0b8ec4b8e93ae14
evbevz <evbevz@gmail.com>
2018-11-17 09:51:01 +0000

    ting build settings

---
+++ Mk/ting.mk
@@ -0,0 +1,14 @@
+CORE_MESSAGE=
+CORE_NAME=		ting
+CORE_TYPE=		release
+
+CORE_COMMENT=		${CORE_PRODUCT} ${CORE_TYPE} package
+CORE_MAINTAINER=	bevz@gmail.com
+CORE_ORIGIN=		${CORE_NAME}
+CORE_PACKAGESITE=	https://update0.smart-soft.ru
+CORE_PRODUCT=		TING
+CORE_WWW=		http://smart-soft.ru/
+
+CORE_COPYRIGHT_HOLDER=	Smart-Soft Ltd.
+CORE_COPYRIGHT_WWW=	http://smart-soft.ru/
+CORE_COPYRIGHT_YEARS=	2016-2018
