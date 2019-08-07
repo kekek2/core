06b0615120f9d0cda78a59f7becaea687cd5de71
Alexander Shursha <kekek2@ya.ru>
2019-08-07 07:32:59 +0300

    Fix newlines in NAT descriptions.

---
+++ src/opnsense/mvc/app/models/SmartSoft/TING/Migrations/M0_0_2.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace SmartSoft\TING\Migrations;
+
+use OPNsense\Base\BaseModelMigration;
+use OPNsense\Core\Config;
+
+
+class M0_0_2 extends BaseModelMigration
+{
+    public function run($model)
+    {
+        parent::run($model);
+
+	$cfg = Config::getInstance();
+	$cfgArray = $cfg->toArray(array_flip(["rule"]));
+	foreach ($cfgArray['nat']['rule'] as $rule) {
+	    $rule['descr'] = preg_replace("/\r|\n/", "", $rule['descr']);
+    }
+	$cfg->fromArray($cfgArray);
+        $cfg->save();
+    }
+}
