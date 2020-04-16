c0f04801fdedf792571ca95833beadf7d38b6d67
Alexander Shursha <kekek2@ya.ru>
2020-04-16 08:05:03 +0300

    Fix flushing NetFlow data.

--- src/opnsense/scripts/netflow/flush_all.sh
+++ src/opnsense/scripts/netflow/flush_all.sh
@@ -2,12 +2,10 @@
 
 if [ "$1" == "all" ]; then
     echo "flush all local netflow data"
-    /usr/local/etc/rc.d/flowd_aggregate stop
-    /usr/local/etc/rc.d/flowd stop
+    /usr/local/etc/rc.syshook.d/stop/75-flowd
     rm  /var/netflow/*.sqlite
     rm /var/log/flowd.log*
-    /usr/local/etc/rc.d/flowd start
-    /usr/local/etc/rc.d/flowd_aggregate start
+    /usr/local/etc/rc.syshook.d/start/25-flowd
 else
     echo "not flushing local netflow data, provide all as parameter to do so"
 fi
